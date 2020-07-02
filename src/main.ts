b8a68164d550edb61e7814c70fd8da9add50f3ea
Jacek Malec <jacek.malec@itv.com>
2020-07-02 15:54:54 +0100

    Cleanup script

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as inquirer from "inquirer";
 import * as ProgressBar from "progress";
 import * as yargs from "yargs";
 import { Space } from "contentful-management/typings/space";
+import { Entry } from "contentful-management/typings/entry";
 
 export async function main() {
     const argv = yargs.env()
@@ -15,7 +16,7 @@ export async function main() {
             describe: "Contentful environment",
             default: "master",
             demandOption: false
-         }).option("accesstoken", {
+        }).option("accesstoken", {
             type: "string",
             describe: "Contentful access token",
             demandOption: true
@@ -39,6 +40,11 @@ export async function main() {
             type: "boolean",
             alias: "v",
             default: false
+        }).option("whitelist", {
+            type: "string",
+            describe: "File with IDs to ignore",
+            alias: "w",
+            default: undefined
         }).version(false)
         .parse();
     const accessToken: string = argv["accesstoken"];
@@ -48,6 +54,7 @@ export async function main() {
     const contentType: string | undefined = argv["content-type"];
     const isContentTypes: boolean = argv["content-types"];
     const yes: boolean = argv["yes"];
+    const whitelist: string | undefined = argv["whitelist"];
 
     const env: string = argv["env"] || 'master';
 
@@ -57,12 +64,17 @@ export async function main() {
     console.log(`Opening Contentful space "${spaceId}"`);
     const contentfulSpace = await contentfulManagementClient.getSpace(spaceId);
     console.log(`Using space "${spaceId}" (${contentfulSpace.name})`);
+    let ignored: Array<string> = [];
+
+    if (whitelist) {
+        ignored = require('fs').readFileSync(whitelist, 'utf-8').split(/\r?\n/);
+    }
 
     if (!yes) {
         if (!await promptForEntriesConfirmation(spaceId, env))
             return;
     }
-    await deleteEntries(contentfulSpace, contentType, batchSize, verbose, env);
+    await deleteEntries(contentfulSpace, contentType, batchSize, verbose, env, e => !ignored.some(id => id == e.sys.id));
 
     if (isContentTypes) {
         if (!yes) {
@@ -91,7 +103,7 @@ async function promptForContentTypesConfirmation(spaceId: string, environment: s
     return a.yes;
 }
 
-async function deleteEntries(contentfulSpace: Space, contentType: string | undefined, batchSize: number, verbose: boolean,  environment: string) {
+async function deleteEntries(contentfulSpace: Space, contentType: string | undefined, batchSize: number, verbose: boolean, environment: string, filter: (e: Entry) => Boolean) {
     const selectedEnvironment = await contentfulSpace.getEnvironment(environment);
     const entriesMetadata = await selectedEnvironment.getEntries({
         include: 0,
@@ -101,34 +113,40 @@ async function deleteEntries(contentfulSpace: Space, contentType: string | undef
     let totalEntries = entriesMetadata.total;
     console.log(`Deleting ${totalEntries} entries`);
 
+    let offset = 0;
     // tslint:disable-next-line:max-line-length
     const entriesProgressBar = new ProgressBar("Deleting entries [:bar], rate: :rate/s, done: :percent, time left: :etas", { total: totalEntries });
     do {
         const entries = await selectedEnvironment.getEntries({
             include: 0,
+            skip: offset,
             limit: batchSize,
             content_type: contentType
         });
         totalEntries = entries.total;
 
         const promises: Array<Promise<void>> = [];
-        for (const entry of entries.items) {
+        for (const entry of entries.items.filter(e => filter(e))) {
             const promise = unpublishAndDeleteEntry(entry, entriesProgressBar, verbose);
             promises.push(promise);
         }
         await Promise.all(promises);
+        offset += entries.limit;
     } while (totalEntries > batchSize);
 }
 
-async function unpublishAndDeleteEntry(entry: any, progressBar: ProgressBar, verbose: boolean) {
+async function unpublishAndDeleteEntry(entry: Entry, progressBar: ProgressBar, verbose: boolean) {
     try {
         if (entry.isPublished()) {
             if (verbose)
                 console.log(`Unpublishing entry "${entry.sys.id}"`);
             await entry.unpublish();
         }
-        if (verbose)
+        if (verbose) {
             console.log(`Deleting entry '${entry.sys.id}"`);
+        }
+        // require('fs').appendFileSync('log.csv', `${entry.fields.programmeNamePcs["en-GB"]}\n`);
+        // require('fs').appendFileSync('log-ids.csv', `${entry.sys.id}\n`);
         await entry.delete();
     } catch (e) {
         console.log(e);
@@ -165,7 +183,7 @@ async function deleteContentTypes(contentfulSpace: any, batchSize: number, verbo
     } while (totalContentTypes > batchSize);
 }
 
-async function unpublishAndDeleteContentType(contentType: any, progressBar: ProgressBar, verbose: boolean,  environment: string) {
+async function unpublishAndDeleteContentType(contentType: any, progressBar: ProgressBar, verbose: boolean, environment: string) {
     try {
         if (contentType.isPublished()) {
             if (verbose)
